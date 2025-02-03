7f0a5a10708e8818c72f1fa589e232fd3c03a99b
unknown <curtisjowen@gmail.com>
2025-02-03 02:52:27 -0500

    issue 24: revamp book detail page

--- cps/static/js/caliBlur.js
+++ cps/static/js/caliBlur.js
@@ -675,7 +675,7 @@ $(".shelforder .col-sm-10 .col-sm-6.col-lg-6.col-xs-6 h2:first").text(shelfText)
 function mobileSupport() {
     if ($(window).width() <= 768) {
         //Move menu to collapse
-        $(".row-fluid > .col-sm-2:first").appendTo(".navbar-collapse.collapse:first");
+        // $(".row-fluid > .col-sm-2:first").appendTo(".navbar-collapse.collapse:first");
         if ($(".sidebar-backdrop").length < 1) {
             $(".navbar-collapse.collapse:first").after("<div class='sidebar-backdrop'></div>");
         }
